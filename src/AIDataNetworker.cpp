2f4f57b0a6187ed69bce7a1377f00212bb0bb68c
roboteampc <roboteamtwente@gmail.com>
2022-03-29 16:32:00 +0200

    WIP3

--- src/AIDataNetworker.cpp
+++ src/AIDataNetworker.cpp
@@ -13,16 +13,58 @@ proto::AIData AIDataToProto(const rtt::AIData& data) {
         proto.add_all_plays(playName);
     }
 
+    for (const auto& [robotId, status] : data.robotStpStatus) {
+        auto protoStatus = proto.add_stp_status();
+        protoStatus->set_robot_id(robotId);
+        protoStatus->set_role_name(status.roleName);
+        protoStatus->set_role_status(status.roleStatus);
+        protoStatus->set_tactic_name(status.tacticName);
+        protoStatus->set_tactic_status(status.tacticStatus);
+        protoStatus->set_skill_name(status.skillName);
+        protoStatus->set_skill_status(status.skillStatus);
+    }
+
+    for (const auto& [robotId, path] : data.robotPaths) {
+        auto protoPath = proto.add_robot_paths();
+        protoPath->set_robot_id(robotId);
+
+        for (const auto& point : path) {
+            auto protoPoint = protoPath->add_points();
+            protoPoint->set_x(point.x);
+            protoPoint->set_y(point.y);
+        }
+    }
+
     return proto;
 }
 
 AIData protoToAIData(const proto::AIData& proto) {
     AIData aiData;
 
-    aiData.currentPlay = proto.current_play();
+    aiData.currentPlay = std::string(proto.current_play());
 
     for (const auto &playName : proto.all_plays()) {
-        aiData.allPlays.push_back(playName);
+        aiData.allPlays.push_back(std::string(playName));
+    }
+
+    for (const auto& protoStatus : proto.stp_status()) {
+        rtt::RobotSTPStatus status = {
+            .roleName = std::string(protoStatus.role_name()),
+            .roleStatus = std::string(protoStatus.role_status()),
+            .tacticName = std::string(protoStatus.tactic_name()),
+            .tacticStatus = std::string(protoStatus.tactic_status()),
+            .skillName = std::string(protoStatus.skill_name()),
+            .skillStatus = std::string(protoStatus.skill_status())
+        };
+        aiData.robotStpStatus.insert({ protoStatus.robot_id(), status });
+    }
+
+    for (const auto& protoPath : proto.robot_paths()) {
+        std::vector<Vector2> points;
+        for (const auto& protoPoint : protoPath.points()) {
+            points.emplace_back(Vector2(protoPoint.x(), protoPoint.y()));
+        }
+        aiData.robotPaths.insert({ protoPath.robot_id(), points});
     }
 
     return aiData;
--- system_tests/AIDataListener.cpp
+++ system_tests/AIDataListener.cpp
@@ -1,20 +1,80 @@
 #include <AIDataNetworker.hpp>
 
+#include <roboteam_utils/Format.hpp>
+
 #include <iostream>
 #include <chrono>
+#include <algorithm>
 
 void onAIData(const rtt::AIData &data) {
-    std::cout << "Data: " << std::endl;
-    std::cout << "- Current play: " << data.currentPlay << std::endl;
-    std::cout << "- Plays size: " << data.allPlays.size() << std::endl;
+
+    // Line 0
+    std::cout << "Plays (" << data.allPlays.size() << "):";
+    for (const auto& playName : data.allPlays) {
+        std::cout << " " << playName;
+    }
+    std::cout << std::endl;
+    // Line 1
+    std::cout << "Current play: " << data.currentPlay << std::endl;
+
+    std::vector<int> mapKeys;
+    for (const auto& [id, _] : data.robotStpStatus) {
+        mapKeys.push_back(id);
+    }
+
+    constexpr int MAX_COLUMNS = 3;
+    for (int i = 0; i < mapKeys.size(); i+=MAX_COLUMNS) {
+        // print roles
+        for (int j = i; j < std::min((int)mapKeys.size(), i + MAX_COLUMNS); j++) {
+            int id = mapKeys.at(j);
+            rtt::RobotSTPStatus status = data.robotStpStatus.at(id);
+
+            std::cout << id << ") R: " << rtt::formatString("%-17s : %7s", status.roleName.c_str(), status.roleStatus.c_str()) << " | ";
+        }
+        std::cout << std::endl;
+
+        // print tactics
+        for (int j = i; j < std::min((int)mapKeys.size(), i + MAX_COLUMNS); j++) {
+            int id = mapKeys.at(j);
+            rtt::RobotSTPStatus status = data.robotStpStatus.at(id);
+            std::cout << "   T: " << rtt::formatString("%-17s : %7s", status.tacticName.c_str(), status.tacticStatus.c_str()) << " | ";
+        }
+        std::cout << std::endl;
+
+        // print skills
+        for (int j = i; j < std::min((int)mapKeys.size(), i + MAX_COLUMNS); j++) {
+            int id = mapKeys.at(j);
+            rtt::RobotSTPStatus status = data.robotStpStatus.at(id);
+            std::cout << "   S: " << rtt::formatString("%-17s : %7s", status.skillName.c_str(), status.skillStatus.c_str()) << " | ";
+        }
+        std::cout << std::endl;
+    }
 }
 
 int main() {
 
     rtt::net::AIPrimaryDataSubscriber subscriber(onAIData);
 
+    //std::cout << "Hello there!" << std::flush;
+    //std::this_thread::sleep_for(std::chrono::seconds(1));
+    //std::cout << "\rGeneral kenobi!" << std::flush;
+
+//    printf("\\033[F test");
+//    fflush(std::cout);
+
+    std::cout << "Hello" << std::endl << "There" << std::endl << "General" << std::endl << "Kenobi" << std::flush;
+//    std::cout << "\033[F\033[Fhello there" << std::flush;
+//    std::cout << "\033[A" << "Test" << std::flush;
+
+
+
+    int i = 0;
+
     while (true) {
         std::this_thread::sleep_for(std::chrono::seconds(1));
-        std::cout << "Tick" << std::endl;
+        //printf("\33[2K\r");
+        //printf("Hello there %d \n", i);
+
+        i++;
     }
 }
